643c2fe1e9ddb62b739985ba0a244c0351ec845e
Valters Veide <valters.veide@gmail.com>
2021-02-01 17:39:07 +0200

    Kods rediģēts, lai prezentētu publiski

---
+++ FARD-0.1.py
@@ -0,0 +1,167 @@
+import fontforge
+import psMat
+from math import radians
+import time
+
+# Import font
+sample_font = fontforge.open("/Users/valtersveide/Desktop/Gilroy-ExtraBold.ttf")
+
+# Create new font
+new_font = fontforge.font()
+
+# Set encoding to Unicode BMP
+new_font.encoding = "ISO10646-1"
+
+new_font.fontname = "newFont#10"
+new_font.familyname = "newFont#10"
+new_font.fullname = "newFont#10"
+#new_font.descent = 10
+#new_font.ascent = 20
+
+# Transfer default letters
+sample_font.selection.select(("ranges", None), 0, 191)
+sample_font.copy()
+
+new_font.selection.select(("ranges", None), 0, 191)
+new_font.paste()
+
+
+# Transfer letter
+def transferletter(letter, where):
+    sample_font.selection.none()
+    sample_font.selection.select(letter)
+    sample_font.copy()
+
+    new_font.selection.none()
+    new_font.selection.select(*where)
+    new_font.paste()
+
+# Transfer glyph
+class TransferGlyph:
+    def __init__(self, glyph):
+        self.ensure_og_state(glyph)
+        self.glyph = glyph
+
+    def change_size(self, glyph, xsize, ysize, xskew, yskew):
+        move = 0
+        new_font.selection.none()
+
+        new_font.selection.select(glyph)
+        new_font.transform([xsize, yskew, xskew, ysize, move, move])
+        new_font.selection.none()
+
+
+
+
+
+
+    def find_middle_x(self, base_layer, paste_layer, xxmove):
+        xmax = base_layer.boundingBox()
+        glyph_size = paste_layer.boundingBox()
+        xmove = (((xmax[2] - xmax[0]) / 2) + xmax[0]) - (((glyph_size[2] - glyph_size[0]) / 2) + glyph_size[0])
+        return xmove * xxmove
+
+    def find_middle_x_at_top_y(self, base_layer, paste_layer, xxmove):
+        topmiddle = self.topy_middle_x(base_layer)
+        glyphmiddle = self.middle_x(paste_layer)
+
+        xmove = topmiddle - glyphmiddle
+        return xmove * xxmove
+
+    def find_middle_glyph_bottom_x_at_top_y(self, base_layer, paste_layer, xxmove):
+        topmiddle = self.topy_middle_x(base_layer)
+        bottommiddle = self.bottomy_middle_x(paste_layer)
+
+        xmove = topmiddle - bottommiddle
+        return xmove * xxmove
+
+    def find_middle_glyph_bottom_x_at_middle_y(self, base_layer, paste_layer, xxmove):
+        topmiddle = self.middle_x(base_layer)
+        bottommiddle = self.bottomy_middle_x(paste_layer)
+
+        xmove = topmiddle - bottommiddle
+        return xmove * xxmove
+
+    def find_left_x_at_bottom_y(self, base_layer, paste_layer, xxmove):
+        xmax = base_layer.boundingBox()
+
+        paste_middle = self.topy_middle_x(paste_layer)
+        base_left = xmax[0]
+        xmove = base_left - paste_middle
+        return xmove * xxmove
+
+    def find_right_x_at_bottom_y(self, base_layer, paste_layer, xxmove):
+        xpos = base_layer.boundingBox()
+        ypos = paste_layer.boundingBox()
+
+        ytop = ypos[3]
+        xtop_bounds = paste_layer.xBoundsAtY(ytop)
+
+        xtop = self.find_nearest_point_top_to_bottom(paste_layer, xtop_bounds, ytop)
+        base_right = xpos[2]
+        xmove = base_right - xtop[1]
+        return xmove * xxmove
+
+    def find_right_x_at_top_y(self, base_layer, paste_layer, coefficient2, xxmove):
+        base = base_layer.boundingBox()
+        paste = paste_layer.boundingBox()
+
+        ytop = base[3]
+        ytop_bounds = base_layer.xBoundsAtY(ytop)
+        base_top = self.find_nearest_point_top_to_bottom(base_layer, ytop_bounds, ytop)
+
+        if base_top[0] < base_top[1]:
+            base_top_point = base_top[1]
+        else:
+            base_top_point = base_top[0]
+
+        paste_whole = paste[2]
+
+        xmove = (base_top_point - paste[0]) + (paste_whole * coefficient2)
+        return xmove * xxmove
+
+##############################################
+##############################################
+############### Kods Dzēsts ##################
+##############################################
+##############################################
+
+upsideE = TransferGlyph(101)
+
+upsideE.rotateGlyph(180, 101)
+upsideE.centerMiddle(1, 1, 0, 0, 1, 1, 477)
+upsideE.transferHard(477)
+##############################################
+
+where_macron_late = [481, 560, 561, 556, 557]
+for character in where_macron_late:
+    plusMacron.centerTop(1, 0.8, 0, 0, 0.40, 1, 1, character)
+    plusMacron.transfer()
+
+where_macron_late_spec = [480]
+for character in where_macron_late_spec:
+    plusMacron.topCenterChar(1, 0.8, 0, 0, 0.40, 1, 1, character)
+    plusMacron.transfer()
+
+where_acute_late = [471, 472, 510, 511]
+for character in where_acute_late:
+    plusAcute.centerTop(1, 1, 0, 0, acutecoef, 1, 1, character)
+    plusAcute.transfer()
+
+where_acute_late_spec = [506, 507]
+for character in where_acute_late_spec:
+    plusAcute.topCenter(1, 1, 0, 0, acutecoef, 1, 1, character)
+    plusAcute.transfer()
+
+where_grave_late = [475, 476]
+for character in where_grave_late:
+    plusGrave.centerTop(1, 1, 0, 0, gravecoef, 1, 1, character)
+    plusGrave.transfer()
+
+
+# Save
+sec = time.localtime()
+stamp = time.strftime("%d-%b | %H:%M:%S", sec)
+new_font.fontname = "SpaceTest"
+new_font.save("/Users/valtersveide/Test " + str(stamp) + ".otf")
+print(stamp)
---
+++ README.md
@@ -0,0 +1,5 @@
+# FARD
+Font Automated Return Of Diacritics
+
+This code is written as a part of my Scientific Research Work (ZPD) in the Riga French Lyceum.
+Using the font-forge python module, the author of this code has written a script that automatically generates letters with diacritic signs in any font given.
